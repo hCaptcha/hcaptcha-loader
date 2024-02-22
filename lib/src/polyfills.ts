bd70315927011625b823c2fb91882025853c6d9c
Faris Mahmutovic <faris.mahmutovic@intuitionmachines.com>
2024-02-22 16:10:09 +0100

    fix: add polyfill

--- lib/src/polyfills.ts
+++ lib/src/polyfills.ts
@@ -1,4 +1,5 @@
 import 'core-js/es/array/find';
+import 'core-js/es/array/includes';
 import 'core-js/es/object/assign';
 import 'core-js/es/object/entries';
 import 'core-js/es/object/get-own-property-descriptors';
