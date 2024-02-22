57876706e9c6d37f7ceacae2945acabdd973e94d
Faris Mahmutovic <115102863+faris-imi@users.noreply.github.com>
2024-02-22 18:16:01 +0100

    fix(sentry): add polyfills and use xhr for es5 (#25)

--- lib/src/polyfills.ts
+++ lib/src/polyfills.ts
@@ -1,4 +1,8 @@
 import 'core-js/es/array/find';
+import 'core-js/es/array/includes';
 import 'core-js/es/object/assign';
 import 'core-js/es/object/entries';
 import 'core-js/es/object/get-own-property-descriptors';
+import 'core-js/es/map';
+import 'core-js/es/set';
+import 'core-js/es/symbol';
