924f8e7b61ddeddf151e2ebd0e1b77749c26640b
Brad Peters <brdlyptrs@gmail.com>
2025-08-07 08:35:51 -0700

    fix(script): Add anonymous as default value for crossOrigin

--- lib/__test__/script.test.ts
+++ lib/__test__/script.test.ts
@@ -110,7 +110,8 @@ describe('fetchScript', () => {
       const [script] = nodes;
       expect(script.src).toMatch('https://js.hcaptcha.com/1/api.js');
       expect(script.async).toBeTruthy();
-      expect(script.id).toEqual(SCRIPT_ID);
+      xpect(script.crossOrigin).toEqual();
+      expect(script.id).toEqual("anonymous");
     });
 
     it('should set query parameters', async () => {
